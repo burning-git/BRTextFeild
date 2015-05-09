40ee452c68b43aa08f78acb0e9b8df60373c82d7
burning_git <974201360@qq.com>
2015-05-09 14:11:10 +0800

    Initial Commit

---
+++ BRTextFeild/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  BRTextFeild
+//
+//  Created by gitBurning on 15/5/9.
+//  Copyright (c) 2015年 BR. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ BRTextFeild/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  BRTextFeild
+//
+//  Created by gitBurning on 15/5/9.
+//  Copyright (c) 2015年 BR. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
